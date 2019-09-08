25e078c771acef84e5400c5c7a5f7f86ef1845df
Beimete <783416419@qq.com>
2019-09-09 07:57:53 +0800

    vue-resource half

--- Node_server/app.js
+++ Node_server/app.js
@@ -1,6 +1,6 @@
 // 导入解析URL地址的核心模块
 const http = require('http');
-// 解析URL地址
+// 解析URL地址，拿到 pathname query
 const urlModule = require('url')
 
 // 创建一个http服务器
@@ -8,11 +8,20 @@ const server = http.createServer();
 
 // 监听服务器的request请求事件，处理每个请求
 server.on('request', function(req, res) {
-    const url = req.url;
+    // const url = req.url;
+    const { pathname:url, query} = urlModule.parse(req.url, true)
     
     if(url === '/getscript'){
         // 拼接一个合法的JavaScript脚本，此处拼接一个方法的调用
-        var scriptStr = 'show()'
+        // var scriptStr = 'show()'
+
+        var data = {
+            name:'twang',
+            age:22,
+            gender:'男孩子',
+
+        }
+        var scriptStr = `${query.callback}(${JSON.stringify(data)})`
         // res.end发送给客户端，客户端把这个字符串当做JavaScript代码去执行
         res.end(scriptStr)
     }else{
