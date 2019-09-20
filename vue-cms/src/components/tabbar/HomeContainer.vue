b64d6ff7b2c76b74f2128bc112c3df08c8809161
Beimete <783416419@qq.com>
2019-09-20 20:18:16 +0800

    vue project begin

---
+++ vue-cms/src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <mt-swipe :auto="4000">
+        <mt-swipe-item v-for="item in lunboList" :key="item.id">
+            <img :src="item.img" alt="">
+        </mt-swipe-item>
+        
+        </mt-swipe>
+       <h3>HomeContainer</h3> 
+    </div>
+</template>
+
+<script>
+import {Toast} from 'mint-ui'
+
+    export default{
+        data(){
+            return {
+               lunboList:[] 
+            }
+        },
+        created(){
+            this.getLunbo()
+        },
+        methods:{
+            getLunbo(){
+                this.$http.get('http://www.liulongbin.top:3005/api/getlunbo').then(
+                    result =>{
+                        if(result.body.status === 0){
+                            this.lunboList = result.body.message;
+                        }else{
+                            Toast('加载轮播图失败...')
+                        }
+                    })
+            }
+        }
+    }
+</script>
+
+<<style lang="scss" scoped>
+    .mint-swipe{
+        height:200px;
+
+        .mint-swipe-item{
+           &:nth-child(1){
+               background-color:red; 
+           };
+           &:nth-child(2){
+               background-color:lightgreen; 
+           };
+           &:nth-child(3){
+               background-color:yellow; 
+           };
+           
+           img{
+               width: 100%;
+               height: 100%;
+           }
+        }
+    }
+</style>
---
+++ vue-cms/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+import app from './App.vue'
+
+import { Header, Swipe, SwipeItem } from 'mint-ui'
+Vue.component(Header.name, Header);
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+import './lib/mui/css/mui.min.css'
+import './lib/mui/css/icons-extra.css'
+
+
+import router from './router.js'
+
+var vm = new Vue({
+    el:"#app",
+    render: c => c(app),
+    router
+})
